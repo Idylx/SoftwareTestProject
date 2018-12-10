f6571c1df779f541ac4073b69fffd42195264c75
oli <olivier.arbellay@students.hevs.ch>
2018-12-10 19:53:41 +0100

    Ajoutez des fichiers projet.

---
+++ InputOutput/InputOutput.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace InputOutput
+{
+    public class InputOutput
+    {
+        //Load image method
+        public Bitmap readImage()
+        {
+             
+            Bitmap loadedImage = null;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select an image file.";
+
+            //Accepted file type
+            ofd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg|Bitmap Images(*.bmp)|*.bmp";
+
+            //Ok button listener
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //Load the file
+                StreamReader streamReader = new StreamReader(ofd.FileName);
+                loadedImage = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+                streamReader.Close();
+            }
+            return loadedImage;
+        }
+
+        //write image method
+        public bool writeImage(Bitmap bitmap)
+        {
+                    
+            bool result = false;
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Select an image file.";
+
+            //Accepted file type
+            sfd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg|Bitmap Images(*.bmp)|*.bmp";
+
+            //Ok button listener
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileExtension = Path.GetExtension(sfd.FileName).ToUpper();
+                ImageFormat imgFormat = ImageFormat.Png;
+
+                //chose format logic
+                switch (fileExtension)
+                {
+                    case "BMP":
+                        imgFormat = ImageFormat.Bmp;
+                        break;
+                    case "PNG":
+                        imgFormat = ImageFormat.Png;
+                        break;
+                    case "JPG":
+                        imgFormat = ImageFormat.Jpeg;
+                        break;
+                }
+
+                //writting image
+                StreamWriter streamWriter = new StreamWriter(sfd.FileName, false);
+                try { 
+                    bitmap.Save(streamWriter.BaseStream, imgFormat);
+                    streamWriter.Flush();
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e);
+                    return false;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return false;
+                }
+                finally
+                {
+                    if (streamWriter != null)
+                    {
+                        streamWriter.Close();
+                        result = true;
+                        bitmap = null;
+                    }
+                }
+                
+            }
+            return result;
+        }
+
+    }
+}
---
+++ SoftwareTestProject/MainForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SoftwareTestProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.picPreview = new System.Windows.Forms.PictureBox();
+            this.btnOpenOriginal = new System.Windows.Forms.Button();
+            this.btnSaveNewImage = new System.Windows.Forms.Button();
+            this.cmbEdgeDetection = new System.Windows.Forms.ComboBox();
+            this.comboFilterApplication = new System.Windows.Forms.ComboBox();
+            this.procedureLabel = new System.Windows.Forms.Label();
+            this.edgeDetectionLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPreview
+            // 
+            this.picPreview.BackColor = System.Drawing.Color.Silver;
+            this.picPreview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.picPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picPreview.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.picPreview.Location = new System.Drawing.Point(12, 12);
+            this.picPreview.Name = "picPreview";
+            this.picPreview.Size = new System.Drawing.Size(600, 600);
+            this.picPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picPreview.TabIndex = 13;
+            this.picPreview.TabStop = false;
+            // 
+            // btnOpenOriginal
+            // 
+            this.btnOpenOriginal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenOriginal.Location = new System.Drawing.Point(11, 620);
+            this.btnOpenOriginal.Name = "btnOpenOriginal";
+            this.btnOpenOriginal.Size = new System.Drawing.Size(120, 76);
+            this.btnOpenOriginal.TabIndex = 15;
+            this.btnOpenOriginal.Text = "Load Image";
+            this.btnOpenOriginal.UseVisualStyleBackColor = true;
+            this.btnOpenOriginal.Click += new System.EventHandler(this.btnOpenOriginal_Click);
+            // 
+            // btnSaveNewImage
+            // 
+            this.btnSaveNewImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveNewImage.Location = new System.Drawing.Point(493, 620);
+            this.btnSaveNewImage.Name = "btnSaveNewImage";
+            this.btnSaveNewImage.Size = new System.Drawing.Size(120, 76);
+            this.btnSaveNewImage.TabIndex = 16;
+            this.btnSaveNewImage.Text = "Save Image";
+            this.btnSaveNewImage.UseVisualStyleBackColor = true;
+            // 
+            // cmbEdgeDetection
+            // 
+            this.cmbEdgeDetection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEdgeDetection.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbEdgeDetection.FormattingEnabled = true;
+            this.cmbEdgeDetection.Items.AddRange(new object[] {
+            "None",
+            "Laplacian 5x5 of Gaussian 5x5 - 1",
+            "Prewitt Grayscale"});
+            this.cmbEdgeDetection.Location = new System.Drawing.Point(316, 658);
+            this.cmbEdgeDetection.Name = "cmbEdgeDetection";
+            this.cmbEdgeDetection.Size = new System.Drawing.Size(170, 32);
+            this.cmbEdgeDetection.TabIndex = 20;
+            // 
+            // comboFilterApplication
+            // 
+            this.comboFilterApplication.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboFilterApplication.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboFilterApplication.FormattingEnabled = true;
+            this.comboFilterApplication.Items.AddRange(new object[] {
+            "None",
+            "Magic Mosaic",
+            "Black and White",
+            "Rainbow Filter"});
+            this.comboFilterApplication.Location = new System.Drawing.Point(137, 658);
+            this.comboFilterApplication.Name = "comboFilterApplication";
+            this.comboFilterApplication.Size = new System.Drawing.Size(170, 32);
+            this.comboFilterApplication.TabIndex = 16;
+            // 
+            // procedureLabel
+            // 
+            this.procedureLabel.AutoSize = true;
+            this.procedureLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.procedureLabel.Location = new System.Drawing.Point(137, 620);
+            this.procedureLabel.Name = "procedureLabel";
+            this.procedureLabel.Size = new System.Drawing.Size(65, 20);
+            this.procedureLabel.TabIndex = 22;
+            this.procedureLabel.Text = "1.Filter";
+            // 
+            // edgeDetectionLabel
+            // 
+            this.edgeDetectionLabel.AutoSize = true;
+            this.edgeDetectionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.edgeDetectionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edgeDetectionLabel.Location = new System.Drawing.Point(315, 620);
+            this.edgeDetectionLabel.Name = "edgeDetectionLabel";
+            this.edgeDetectionLabel.Size = new System.Drawing.Size(146, 20);
+            this.edgeDetectionLabel.TabIndex = 23;
+            this.edgeDetectionLabel.Text = "2.Edge detection";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(624, 706);
+            this.Controls.Add(this.btnSaveNewImage);
+            this.Controls.Add(this.edgeDetectionLabel);
+            this.Controls.Add(this.procedureLabel);
+            this.Controls.Add(this.comboFilterApplication);
+            this.Controls.Add(this.cmbEdgeDetection);
+            this.Controls.Add(this.btnOpenOriginal);
+            this.Controls.Add(this.picPreview);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Edge Detection";
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        private System.Windows.Forms.PictureBox picPreview;
+        private System.Windows.Forms.Button btnOpenOriginal;
+        private System.Windows.Forms.Button btnSaveNewImage;
+        private System.Windows.Forms.ComboBox cmbEdgeDetection;
+        private System.Windows.Forms.ComboBox comboFilterApplication;
+        private System.Windows.Forms.Label procedureLabel;
+        private System.Windows.Forms.Label edgeDetectionLabel;
+    }
+}
+
